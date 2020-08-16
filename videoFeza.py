e0eaccfe158ab694b8aef9af14a37e2994153b7e
artogahr <ertogrulgehremanli@gmail.com>
2020-08-16 19:24:37 +0300

    first commit

---
+++ videoFeza.py
@@ -0,0 +1,89 @@
+# import the necessary packages
+from __future__ import division
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import threading
+import time
+import cv2
+import socket
+import struct
+import math
+# initialize the camera and grab a reference to the raw camera capture
+
+command_ip = ''
+command_port = 5005
+
+class FrameSegment(object):
+    """ 
+    Object to break down image frame segment
+    if the size of image exceed maximum datagram size 
+    """
+    MAX_DGRAM = 2**16
+    MAX_IMAGE_DGRAM = MAX_DGRAM - 64  # extract 64 bytes in case UDP frame overflown
+
+    def __init__(self, sock, port, addr="192.168.1.107"):
+        self.s = sock
+        self.port = port
+        self.addr = addr
+
+    def udp_frame(self, img):
+        """ 
+        Compress image and Break down
+        into data segments 
+        """
+        compress_img = cv2.imencode('.jpg', img)[1]
+        dat = compress_img.tostring()
+        size = len(dat)
+        count = math.ceil(size/(self.MAX_IMAGE_DGRAM))
+        array_pos_start = 0
+        while count:
+            array_pos_end = min(size, array_pos_start + self.MAX_IMAGE_DGRAM)
+            self.s.sendto(struct.pack("B", count) +
+                          dat[array_pos_start:array_pos_end],
+                          (self.addr, self.port)
+                          )
+            array_pos_start = array_pos_end
+            count -= 1
+
+
+def main():
+
+    listenerThread = threading.Thread(target=udpListener)
+    listenerThread.setDaemon(True)
+    listenerThread.start()
+
+    port = 12345
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    fs = FrameSegment(sock, port)
+    
+    vid = cv2.VideoCapture("video.mp4")
+    height, width = None, None
+
+    time.sleep(0.1)
+    # capture frames from the camera
+    while True:
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        grabbed, image = vid.read()
+        if not grabbed:
+            break
+        # show the frame
+        fs.udp_frame(image)
+        cv2.imshow("Frame", image)
+        time.sleep(0.1)
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+def udpListener():
+    print("udp listener thread started")
+    comsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    comsock.bind((command_ip, command_port))
+    while True:
+        data, addr = comsock.recvfrom(1024)
+        print("command: %s" % str(data).encode('utf-8'))
+
+if __name__ == "__main__":
+    main()
